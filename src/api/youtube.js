5d6928b958ae7db88ed0d4c4442fcfff45e0c48b
elena-dragunova <elenak-8647@gmail.com>
2019-12-17 10:54:48 +0500

    Add SearchBar components for displaying videos.

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBZN1cjnk0gPeVpkm2jwHhYN7SiuwC-0Xw';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
